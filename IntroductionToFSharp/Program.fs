a13d980deb6b958039f86d919a8e0d909f752cbc
Alexander Sidorenko <xarendel@gmail.com>
2025-03-19 14:34:38 +0300

    Task 1.2 (Added method for solving quadratic equation)

--- IntroductionToFSharp/Program.fs
+++ IntroductionToFSharp/Program.fs
@@ -1,7 +1,25 @@
 ﻿open System
 
+type SolveResult =
+    None
+    | Linear of float
+    | Quadratic of float*float
+
+let solve a b c =
+    let D = b * b - 4.0 * a * c
+    if a = 0.0 then
+        if b = 0.0 then None
+        else Linear(-c / b)
+    else
+        if D < 0.0 then None
+        else Quadratic((((-b + sqrt D) / (2.0 * a)), ((-b - sqrt D) / (2.0 * a))))
+
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World!"
-    Console.WriteLine("Hello World!")
+    let result = solve 3.0 -4.0 2.0
+    match result with
+        None -> Console.WriteLine("No solutions.")
+        | Linear(x) -> Console.WriteLine($"Linear equation: {x}.")
+        | Quadratic(x1, x2) -> Console.WriteLine($"Quadratic equation: {x1}, {x2}.")
     0
