5acb1fd3c1e79fd7c7de36a2f5dc62c3e322ac7f
Alexander Sidorenko <xarendel@gmail.com>
2025-03-26 14:17:24 +0300

    Task 1.6.2 (Added function that takes an argument and returns function)

--- HigherOrderFunctions&Recursion/Program.fs
+++ HigherOrderFunctions&Recursion/Program.fs
@@ -13,8 +13,22 @@ let rec fibDown number =
             | _ -> fibLoop now (now + pred) (num - 1) 
     fibLoop 1 0 number
 
+let rec sumOfDigitsDown number =
+    let rec sumDigitsDownLoop number sum =
+        if number = 0 then sum
+        else
+            let newNumber = number / 10
+            let digit = number % 10
+            let newSum = sum + digit
+            sumDigitsDownLoop newNumber newSum
+    sumDigitsDownLoop number 0
+
+let taskNumberSix flag =
+    match flag with
+        | true -> sumOfDigitsDown
+        | false -> fibDown
+
 [<EntryPoint>]
 let main argv =
-    Console.WriteLine(fibUp 19)
-    Console.WriteLine(fibDown 19)
+    Console.WriteLine((taskNumberSix false) 19)
     0
