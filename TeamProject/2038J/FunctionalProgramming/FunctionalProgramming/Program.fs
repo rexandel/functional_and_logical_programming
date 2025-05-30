19175f2bfe4e105674844ab2d2bbc37d6c8f3846
Alexander Sidorenko <xarendel@gmail.com>
2025-05-30 09:41:24 +0300

    TM (Added visual studio project of task 2038J on F#)

---
+++ TeamProject/2038J/FunctionalProgramming/FunctionalProgramming/Program.fs
@@ -0,0 +1,34 @@
+﻿open System
+
+type Event =
+    | Bus of int
+    | People of int
+
+let parseEvent (s: string) =
+    let parts = s.Split(' ')
+    match parts.[0] with
+    | "B" -> Bus (int parts.[1])
+    | "P" -> People (int parts.[1])
+    | _ -> failwith "Unknown event type"
+
+let processEvents events =
+    let rec loop remainingPeople acc = function
+        | [] -> List.rev acc
+        | event :: rest ->
+            match event with
+            | People p -> loop (remainingPeople + p) acc rest
+            | Bus b ->
+                let newPeople = max 0 (remainingPeople - b)
+                let canBoard = if b > remainingPeople then "YES" else "NO"
+                loop newPeople (canBoard :: acc) rest
+    loop 0 [] events
+
+[<EntryPoint>]
+let main argv =
+    let n = Console.ReadLine() |> int
+    let events = 
+        List.init n (fun _ -> Console.ReadLine())
+        |> List.map parseEvent
+    let results = processEvents events
+    results |> List.iter (printfn "%s")
+    0
