b614159d3cbcb5aa3fde1fd5a8990b4f8c849b11
Alexander Sidorenko <xarendel@gmail.com>
2025-05-30 09:32:34 +0300

    TM (Added visual studio project of task 2038N on C#)

---
+++ TeamProject/2038N/ImperativeProgramming/ImperativeProgramming/Program.cs
@@ -0,0 +1,25 @@
+﻿public static class ExpressionFixer
+{
+    public static string FixExpression(string input)
+    {
+        if (input.Length != 3)
+            throw new ArgumentException("Input must be 3 characters long");
+
+        char[] chars = input.ToCharArray();
+
+        if (chars[0] < chars[2])
+        {
+            chars[1] = '<';
+        }
+        else if (chars[0] > chars[2])
+        {
+            chars[1] = '>';
+        }
+        else
+        {
+            chars[1] = '=';
+        }
+
+        return new string(chars);
+    }
+}
