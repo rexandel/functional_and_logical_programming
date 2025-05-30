469d9bd4c59e562d87a0c8c0a5c065c2d7252ee8
Alexander Sidorenko <xarendel@gmail.com>
2025-05-30 09:43:27 +0300

    TM (Added tests for task 2038L on C#)

---
+++ TeamProject/2038L/ImperativeProgrammingTests/ImperativeProgrammingTests/BoardCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImperativeProgrammingTests
+{
+    public static class BoardCalculator
+    {
+        public static int CalculateBoards(int n)
+        {
+            if (n == 1)
+            {
+                return 2;
+            }
+
+            int result = n / 2;
+            int remaining18 = n - (n / 2);
+
+            if (n % 2 != 0)
+            {
+                result++;
+                remaining18 -= 2;
+            }
+
+            if (remaining18 > 0)
+            {
+                result += remaining18 / 3;
+                remaining18 %= 3;
+            }
+
+            result += (remaining18 + n + 1) / 2;
+
+            return result;
+        }
+    }
+}
---
+++ TeamProject/2038L/ImperativeProgrammingTests/ImperativeProgrammingTests/Tests.cs
@@ -0,0 +1,84 @@
+﻿namespace ImperativeProgrammingTests
+{
+    [TestClass]
+    public sealed class Tests
+    {
+        [TestMethod]
+        public void CalculateBoards_n1()
+        {
+            Assert.AreEqual(2, BoardCalculator.CalculateBoards(1));
+        }
+
+        [TestMethod]
+        public void CalculateBoards_n2()
+        {
+            Assert.AreEqual(3, BoardCalculator.CalculateBoards(2));
+        }
+
+        [TestMethod]
+        public void CalculateBoards_n3()
+        {
+            Assert.AreEqual(4, BoardCalculator.CalculateBoards(3));
+        }
+
+        [TestMethod]
+        public void CalculateBoards_n4()
+        {
+            Assert.AreEqual(5, BoardCalculator.CalculateBoards(4));
+        }
+
+        [TestMethod]
+        public void CalculateBoards_n5()
+        {
+            Assert.AreEqual(6, BoardCalculator.CalculateBoards(5));
+        }
+
+        [TestMethod]
+        public void CalculateBoards_n6()
+        {
+            Assert.AreEqual(7, BoardCalculator.CalculateBoards(6));
+        }
+
+        [TestMethod]
+        public void CalculateBoards_n7()
+        {
+            Assert.AreEqual(9, BoardCalculator.CalculateBoards(7));
+        }
+
+        [TestMethod]
+        public void CalculateBoards_n8()
+        {
+            Assert.AreEqual(10, BoardCalculator.CalculateBoards(8));
+        }
+
+        [TestMethod]
+        public void CalculateBoards_n9()
+        {
+            Assert.AreEqual(11, BoardCalculator.CalculateBoards(9));
+        }
+
+        [TestMethod]
+        public void CalculateBoards_n10()
+        {
+            Assert.AreEqual(12, BoardCalculator.CalculateBoards(10));
+        }
+
+        [TestMethod]
+        public void CalculateBoards_n20_Returns24()
+        {
+            Assert.AreEqual(24, BoardCalculator.CalculateBoards(20));
+        }
+
+        [TestMethod]
+        public void CalculateBoards_n100_Returns117()
+        {
+            Assert.AreEqual(117, BoardCalculator.CalculateBoards(100));
+        }
+
+        [TestMethod]
+        public void CalculateBoards_n1000_Returns1167()
+        {
+            Assert.AreEqual(1167, BoardCalculator.CalculateBoards(1000));
+        }
+    }
+}
