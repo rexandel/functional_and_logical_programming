947af17e3b867e92f05255bd4de381350743ddf4
Alexander Sidorenko <xarendel@gmail.com>
2025-05-30 09:39:04 +0300

    TM (Added solution of 2038J task on C#)

---
+++ TeamProject/2038J/2038J.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        long w = 0;
+        int t = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < t; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+            string s = input[0];
+            long x = long.Parse(input[1]);
+            
+            if (s == "B")
+            {
+                w -= x;
+                Console.WriteLine(w < 0 ? "YES" : "NO");
+                w = w > 0 ? w : 0;
+            }
+            else
+            {
+                w += x;
+            }
+        }
+    }
+}
