80892522f0b697815a7a45e3608ef570ab9becb1
Alexander Sidorenko <xarendel@gmail.com>
2025-05-30 09:35:18 +0300

    TM (Added tests for task 2038N on C#)

---
+++ TeamProject/2038N/ImperativeProgrammingTests/ImperativeProgrammingTests/ExpressionFixer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImperativeProgrammingTests
+{
+    public static class ExpressionFixer
+    {
+        public static string FixExpression(string input)
+        {
+            char[] chars = input.ToCharArray();
+
+            if (chars[0] < chars[2])
+            {
+                chars[1] = '<';
+            }
+            else if (chars[0] > chars[2])
+            {
+                chars[1] = '>';
+            }
+            else
+            {
+                chars[1] = '=';
+            }
+
+            return new string(chars);
+        }
+    }
+}
